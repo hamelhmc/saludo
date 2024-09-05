73792342052d6ffbc464ba05cf9a40fc5a173cd2
Hamilton Mercado Cuellar <hmercado@colaborador.elcorteingles.es>
2024-09-05 22:29:19 +0200

    chore: Update HTML metadata and add SEO tags for ClimaFlow application

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [vue()],
-  envPrefix: 'VITE_', // Asegúrate de que las variables empiecen con "VITE_"
-  base: '/saludo/',
+  envPrefix: 'VITE_', // Las variables de entorno deben empezar con "VITE_"
+  base: '/saludo/', // Configuración del subdirectorio base
 });
